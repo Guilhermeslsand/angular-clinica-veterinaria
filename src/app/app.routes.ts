71b10a14561adb3e8a3989ed9130d07086f12e16
Guilherme Andrade <guilhermeslandrade@gmail.com>
2026-09-10 14:59:58 -0300

    Create routers in angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'consultas',
+        pathMatch: 'full'
+    },
+    {
+        path: 'consultas',
+        loadComponent: () =>
+            import('./features/consultas/consulta-list/consulta-list')
+                .then(m => m.ConsultaList)
+    },
+    {
+        path: 'consultas/nova',
+        loadComponent: () =>
+            import('./features/consultas/consulta-form/consulta-form')
+                .then(m => m.ConsultaForm)
+    },
+    {
+        path: 'consultas/:id',
+        loadComponent: () =>
+            import('./features/consultas/consulta-detail/consulta-detail')
+                .then(m => m.ConsultaDetail)
+
+    },
+    {
+        path: 'consultas/:id/editar',
+        loadComponent: () =>
+            import('./features/consultas/consulta-form/consulta-form')
+                .then(m => m.ConsultaForm)
+    },
+];
